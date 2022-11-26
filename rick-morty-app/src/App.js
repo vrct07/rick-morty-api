55329b0cab3124bb6f41f189488898f0b3c7c7ed
Vitor Costa <vrct07@gmail.com>
2022-11-26 16:33:18 -0300

    Inicio do front

--- rick-morty-app/src/App.js
+++ rick-morty-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-4">
+        Rick & Morty <span className="text-primary">Wiki</span>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3"></div>
+          <div className="col-8"></div>
+        </div>
+      </div>
+
     </div>
   );
 }
