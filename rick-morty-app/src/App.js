2d8d1714522720bb32d2aba33b2c9ae72d78e3d8
Vitor Costa <vrct07@gmail.com>
2022-11-27 11:12:38 -0300

    Criando os componentes

--- rick-morty-app/src/App.js
+++ rick-morty-app/src/App.js
@@ -1,16 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center ubuntu my-4">
+      <h1 className="text-center my-4">
         Rick & Morty <span className="text-primary">Wiki</span>
       </h1>
 
       <div className="container">
         <div className="row">
-          <div className="col-3"></div>
+          <div className="col-3">
+            <Filters/>
+          </div>
           <div className="col-8"></div>
         </div>
       </div>
