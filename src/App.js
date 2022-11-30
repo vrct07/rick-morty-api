c14fc8c4f323b1f869f4a1e0835cecf2e944102d
Vitor Costa <vrct07@gmail.com>
2022-11-30 19:43:35 -0300

    integração com api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 
 function App() {
+  
+  let [pageNumber, setPageNumber] = useState(1);
+  let pageNumber = 7;
+  let api = `https://rickandmortyapi.com/api/character?page=${pageNumber}`;
+
+  useEffect(() => {
+
+
+  },[api])
+
   return (
     <div className="App">
-      <h1 className="text-center my-4">
+      <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">Wiki</span>
       </h1>
 
       <div className="container">
         <div className="row">
           <div className="col-3">
-            <Filters/>
+            <Filters />
           </div>
           <div className="col-8">
             <div className="row">
-            <div className="col-4">Cards</div>
-            <div className="col-4">Cards</div>
-            <div className="col-4">Cards</div>
+              <Cards />
+              <Cards />
+              <Cards />
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
